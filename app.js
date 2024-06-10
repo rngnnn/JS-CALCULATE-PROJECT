5cfb09a4dbda9d4e8c6fc5dae420d9c18ddf04ff
rngnnn <rngnnnclk@gmail.com>
2024-06-10 09:42:49 +0200

    CALCULATE

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const num=document.querySelectorAll(".num")
+const operator=document.querySelectorAll(".operator")
+const previous=document.querySelector(".previous-display")
+const current=document.querySelector(".current-display")
+const equal=document.querySelector(".equal")
+
+
+num.forEach((Number)=>{
+
+Number.onclick=()=>{
+    altekranText=number.textContent
+    current.textContent=altekranText
+
+}
+
+})
